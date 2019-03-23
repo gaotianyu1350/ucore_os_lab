1eb81323566ce97659130fa382e25e96338349ec
gaotianyu1350 <gaotianyu1350@126.com>
2019-03-23 15:24:13 +0800

    finish lab7

--- labcodes/lab7/kern/trap/trap.c
+++ labcodes/lab7/kern/trap/trap.c
@@ -54,6 +54,10 @@ idt_init(void) {
      *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
      *     Notice: the argument of lidt is idt_pd. try to find it!
      */
+     /* LAB5 2016011348 */ 
+     //you should update your lab1 code (just add ONE or TWO lines of code), let user app to use syscall to get the service of ucore
+     //so you should setup the syscall interrupt gate in here
+
     // (1)
     extern uintptr_t __vectors[];
     // (2)
